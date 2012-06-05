44cc8e9e2863207f84ff445858417395f72cdffe
Michel Käser <michel.kaeser@impres-sign.com>
2012-06-05 23:01:24 +0200

    * Fixed current_class not working with nav:auto

--- system/fizl/plugins/nav.php
+++ system/fizl/plugins/nav.php
@@ -29,103 +29,103 @@ class Plugin_nav extends Plugin {
 	public function nav()
 	{
 		$this->CI = get_instance();
-	
+
 		if(!$content = trim($this->tag_content)) return;
-	
+
 		// Break into lines
 		$lines = explode("\n", $content);
-		
+
 		// We are going to put things into an
 		// an array first
 		$nav_arr = array();
-		
+
 		$this->level = 0;
-		
+
 		$html = '<ul class="'.$this->get_param('ul_class', 'nav').'">'."\n";
-		
+
 		foreach($lines as $key => $line):
 
 			$line = trim($line);
 
 			if($line[0] == '-'):
-	
+
 				// Get the data
 				$items = explode(" ", trim($line), 2);
-				
+
 				// This has some sort of sub level
 				$tmp_level = substr_count(trim($items[0]), '-');
 				$link_data = $items[1];
-				
+
 			else:
-			
+
 				// This is a top level item
 				$tmp_level = 0;
 				$link_data = $line;
-			
+
 			endif;
-							
+
 			// Set the level. Has it changed?
 			if($this->level < $tmp_level):
-			
+
 				// We are stepping into a deeper level.
 				// We need to not close off the LI and
 				// start a new ul
 				$html .= "\n<ul>\n";
-								
+
 			endif;
 
 			if($this->level > $tmp_level):
-				
+
 				// We are stepping into a shallower level.
-				// Close off the ul and the li and 
+				// Close off the ul and the li and
 				$html .= "</li>\n</ul>\n";
-				
+
 			endif;
-			
+
 			$html .= '<li class="';
-			
+
 			// Looks like we just a single item
 			$pieces = explode('|', trim($link_data), 2);
-			
+
 			// Get the current string
 			$uri_segs = explode('/', $pieces[0]);
-			
+
 			$popped_uri = array_slice($this->CI->uri->segment_array(), 0, count($uri_segs));
-			
+
 			// Is the current link?
 			if($pieces[0] == implode('/', $popped_uri)):
-			
+
 				$html .= trim($this->get_param('current_class', 'current')).' ';
-			
+
 			endif;
-			
+
 			// Creat link
 			$html .= 'level_'.$tmp_level.'"><a href="';
-						
+
 			if(strpos($pieces[0], 'http://')!==FALSE or strpos($pieces[0], 'https://')!==FALSE):
-			
+
 				$html .= $pieces[0];
-			
+
 			else:
-			
+
 				$html .= site_url($pieces[0]);
-			
+
 			endif;
 
 			$html .= '">'.$pieces[1].'</a>';
-			
+
 			// Does is the next l
-			if(isset($lines[$key+1]) and trim($lines[$key+1][0]) != '-') 
+			if(isset($lines[$key+1]) and trim($lines[$key+1][0]) != '-')
 				$html .= "</li>\n";
 
 			$this->level = $tmp_level;
-							
+
 		endforeach;
-		
+
 		return $html .= "</li>\n</ul>";
 	}
 
-	// --------------------------------------------------------------------------   
+	// --------------------------------------------------------------------------
 
 	/**
 	 * Auto Nav
@@ -143,12 +143,12 @@ class Plugin_nav extends Plugin {
 		$start = $this->get_param('start', '/');
 		$depth = $this->get_param('depth', 2);
 		$remove_index = $this->get_param('remove_index', 'yes');
-		
+
 		$this->remove_index = ($remove_index == 'yes') ? TRUE : FALSE;
-		
+
 		// We need a start
 		if ( ! $start) return NULL;
-		
+
 		if ($start == '/')
 		{
 			$segs = array();
@@ -157,37 +157,37 @@ class Plugin_nav extends Plugin {
 		{
 			$segs = explode('/', $start);
 		}
-		
+
 		$this->CI->load->helper(array('file', 'directory'));
-		
+
 		$this->start = $start;
-						
+
 		$map = directory_map(FCPATH.$this->CI->config->item('site_folder').'/'.$start, $depth);
-		
+
 		if( ! $map) return NULL;
 
 		// ----------------------------------
 		// Parse Directory Map into array
 		// ----------------------------------
-				
+
 		$this->stack = $segs;
 		$new_map = $this->_parse_map_row($map);
-		
+
 		// ----------------------------------
 		// Create the UL from the array
 		// ----------------------------------
-		
+
 		$this->depth = 0;
 		$this->stack = $segs;
-		
+
 		$this->html = '';
 		$this->create_ul($new_map);
-		
+
 		return $this->html;
 	}
 
-	// --------------------------------------------------------------------------   
-	
+	// --------------------------------------------------------------------------
+
 	/**
 	 * Parse Map Row
 	 *
@@ -202,24 +202,24 @@ class Plugin_nav extends Plugin {
 	{
 		$new_map = array();
 		$order = FALSE;
-		
+
 		// First, do we have an order.txt? If so, let's load
 		// it up into an array.
 		if (in_array('order.txt', $map))
-		{					
+		{
 			$path = implode('/', array_merge(array(FCPATH.$this->CI->config->item('site_folder')), $this->stack));
-			
+
 			if (is_file($path.'/order.txt'))
 			{
 				$order = trim(read_file($path.'/order.txt'));
-				
+
 				// chop it up
 				$ord = explode("\n", $order);
-								
+
 				foreach ($ord as $order_string)
 				{
 					$pieces = explode('|', $order_string);
-					
+
 					if (count($pieces) == 2)
 					{
 						$file = trim($pieces[0]);
@@ -230,15 +230,15 @@ class Plugin_nav extends Plugin {
 						$file = $order_string;
 						$name = $this->guess_name($file);
 					}
-					
+
 					// Find the map item. If it is an array key,
 					// that means it s a folder.
 					if (array_key_exists($file, $map))
 					{
-	        			$this->stack[] = $file;
-						
+								$this->stack[] = $file;
+
 						$new_map[$file] = array_merge(array('_title' => $name), $this->_parse_map_row($map[$file]));
-						
+
 						array_pop($this->stack);
 					}
 					else
@@ -255,9 +255,9 @@ class Plugin_nav extends Plugin {
 				if (is_array($file))
 				{
 					$this->stack[] = $key;
-				
+
 					$new_map[$key] = array_merge(array('_title' => $this->guess_name($key)), $this->_parse_map_row($map[$key]));
-					
+
 					array_pop($this->stack);
 				}
 				else
@@ -265,17 +265,17 @@ class Plugin_nav extends Plugin {
 					$new_map[$this->remove_extension($file)] = $this->guess_name($file);
 				}
 			}
-			
+
 			if ($this->remove_index === TRUE AND isset($new_map['index']))
 			{
 				unset($new_map['index']);
 			}
 		}
 
-		return $new_map;		
+		return $new_map;
 	}
 
-	// --------------------------------------------------------------------------   
+	// --------------------------------------------------------------------------
 
 	/**
 	 * Create HTML UL from tree array
@@ -287,42 +287,54 @@ class Plugin_nav extends Plugin {
 	public function create_ul($tree)
 	{
 		$this->depth++;
-	
+
 		$this->html .= '<ul class="depth_'.$this->depth.'">'."\n";
-		
-	    foreach($tree as $key => $item)
-	    {
-	    	if ($key == '_title') continue;
-	    
-	        if (is_array($item))
-	        {
-	        	$this->stack[] = $key;
 
-	            $this->html .= '<li><a href="'.site_url(implode('/', $this->stack)).'">'.$item['_title']."</a>\n";
-	        	
-	            $this->create_ul($item);
-	            
-	            $this->html .= '</li>';
-	            
-	        	array_pop($this->stack);
+			foreach($tree as $key => $item)
+			{
+				if ($key == '_title') continue;
+
+					if (is_array($item))
+					{
+						$this->stack[] = $key;
+
+							$this->html .= '<li><a href="'.site_url(implode('/', $this->stack)).'">'.$item['_title']."</a>\n";
+
+							$this->create_ul($item);
+
+							$this->html .= '</li>';
+
+						array_pop($this->stack);
 			}
-	        else
-	       	{
-	    		$this->stack[] = $key;
-	        
-	            $this->html .= "\t".'<li><a href="'.site_url(implode('/', $this->stack)).'">'.$item.'</a></li>'."\n";
+					else
+					 {
+					$this->stack[] = $key;
 
-	        	array_pop($this->stack);
-	        }
-		}	        
+							// is it the current one?
+							if ($key == uri_string()) {
+
+								// get current_class parameter
+								$current_class = trim($this->get_param('current_class', 'current'));
+
+								$this->html .= "\t".'<li class="'. $current_class .'"><a href="'.site_url(implode('/', $this->stack)).'">'.$item.'</a></li>'."\n";
+
+							} else {
+
+								$this->html .= "\t".'<li><a href="'.site_url(implode('/', $this->stack)).'">'.$item.'</a></li>'."\n";
+
+							}
+
+						array_pop($this->stack);
+					}
+		}
 
 		$this->html .= '</ul>'."\n\n";
 
 		$this->depth--;
 	}
 
-	// --------------------------------------------------------------------------   
-	
+	// --------------------------------------------------------------------------
+
 	/**
 	 * Guess Name
 	 *
@@ -336,15 +348,15 @@ class Plugin_nav extends Plugin {
 	public function guess_name($name)
 	{
 		$name = $this->remove_extension($name);
-	
+
 		$name = str_replace('-', ' ', $name);
 		$name = str_replace('_', ' ', $name);
-		
+
 		return ucwords($name);
 	}
 
-	// --------------------------------------------------------------------------   
-	
+	// --------------------------------------------------------------------------
+
 	/**
 	 * Remove the extension from a file
 	 *
@@ -355,14 +367,14 @@ class Plugin_nav extends Plugin {
 	public function remove_extension($file)
 	{
 		$segs = explode('.', $file, 2);
-		
+
 		if(count($segs) > 1)
 		{
 			array_pop($segs);
 			$file = implode('.', $segs);
 		}
-		
+
 		return $file;
 	}
-		
+
 }
